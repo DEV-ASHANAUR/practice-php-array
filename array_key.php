155a7b10c5e361165f5f030328be37b81707e2aa
dev-ashanaur <devashanaur009@gmail.com.com>
2021-06-01 21:51:09 +0600

    learn array key

---
+++ array_key.php
@@ -0,0 +1,14 @@
+<?php
+// array_key() use for see all key in a array
+// array_key_first() use for see first key in a array
+// array_key_last() use for see last key in a array
+//array_key_exists('key',$array) use for search key in a array
+//key_exists() use for search key in a array
+
+$array = ['red','green','blue','brown'];
+
+print_r(array_keys($array));
+
+
+
+?>
